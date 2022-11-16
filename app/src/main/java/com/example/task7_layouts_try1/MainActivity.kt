0db5e12a0e78aed6c1af56c0e77a9dcad9031585
casa <sacadmedeperu@gmail.com>
2022-11-16 21:31:25 +0100

    commit finalizadopulido

--- app/src/main/java/com/example/task7_layouts_try1/MainActivity.kt
+++ app/src/main/java/com/example/task7_layouts_try1/MainActivity.kt
@@ -11,13 +11,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val estado_Civil_Spinner = findViewById<Spinner>(R.id.spinner1)
-        val estado_Civil_Lista = listOf("Soltero", "Casado")
-        val adaptador = ArrayAdapter(this,android.R.layout.simple_spinner_item,estado_Civil_Lista)
-        estado_Civil_Spinner.adapter = adaptador
+        val estado_Civil_Lista = listOf("Soltero", "Casado", "Separado", "Divorciado")
+        estado_Civil_Spinner.adapter = ArrayAdapter(this,android.R.layout.simple_spinner_item,estado_Civil_Lista)
 
         val cargo_Spinner = findViewById<Spinner>(R.id.spinner2)
         val cargo_Lista = listOf("Ventas", "Informática", "Jardinería")
-        val adaptador2 = ArrayAdapter(this,android.R.layout.simple_spinner_item,cargo_Lista)
-        cargo_Spinner.adapter = adaptador2
+        cargo_Spinner.adapter = ArrayAdapter(this,android.R.layout.simple_spinner_item,cargo_Lista)
     }
 }
